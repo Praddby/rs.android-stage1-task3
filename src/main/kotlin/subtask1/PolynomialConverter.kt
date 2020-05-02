f557cb9fa9d3a69cdd6fc9b08f125d4d587100e1
Yury Bialou <Pradd.by@gmail.com>
2020-05-02 14:38:45 +0300

    feat: add fun subtask 1

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,27 @@
 package subtask1
 
+import kotlin.math.abs
+
 class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isEmpty()) return null
+        var result = ""
+        numbers.forEachIndexed { index, i ->
+            var operator = if (i > 0) "+" else "-"
+            if (index == 0) operator = ""
+            val operand = when (index) {
+                numbers.lastIndex -> ""
+                numbers.lastIndex - 1 -> "x"
+                else -> "x^${numbers.lastIndex - index}"
+            }
+            result += when (abs(i))  {
+                0 -> ""
+                1 -> " $operator $operand"
+                else -> " $operator ${abs(i)}$operand"
+            }
+        }
+        return result.trim()
     }
 }
