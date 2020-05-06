fbf3b9f0601a33a236e371ce98359f87aada862e
Yury Bialou <Pradd.by@gmail.com>
2020-05-06 22:48:47 +0300

    fix: fun subtask 2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -1,21 +1,39 @@
 package subtask2
 
-class Combinator {
+import java.math.BigInteger
 
+class Combinator {
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
+        println(array[0])
+        print(array[1])
         var result: Int? = null
         for (n in 1..array[1]) {
-            if (factorial(array[1])/factorial(n) * factorial(n-array[1]) == array[0]) {
-                result = n
-                break
+            if (n < 10) {
+                val sum = factorial(array[1]) / (factorial(n) * factorial(array[1] - n))
+                if (sum == array[0]) {
+                    result = n
+                    break
+                }
+            } else {
+                val sum1 = factorialB(array[1]) / (factorialB(n) * factorialB(array[1] - n))
+                if (sum1.toInt() == array[0]) {
+                    result = n
+                    break
+                }
             }
         }
         return result
     }
 
-    private fun factorial(n: Int): Int {
-        if(n == 0 || n == 1) return 1
+    private fun factorial(n: Int ): Int  {
+        if(n == 0 || n == 1) return n
         return n * factorial(n-1)
+    }
+
+    private fun factorialB(n: Int): BigInteger {
+        var ret = BigInteger.ONE
+        for (i in 1..n) ret = ret.multiply(BigInteger.valueOf(i.toLong()))
+        return ret
     }
 }
