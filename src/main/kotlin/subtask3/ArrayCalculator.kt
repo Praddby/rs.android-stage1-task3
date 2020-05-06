40c6fe879f5c3a503e3ff769af625a0592ecc639
Yury Bialou <Pradd.by@gmail.com>
2020-05-06 20:14:58 +0300

    feat: add fun subtask 3

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,27 @@
 package subtask3
 
+import kotlin.math.abs
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var arrayInt = itemsFromArray.filterIsInstance<Int>()
+        if (arrayInt.isEmpty()) return 0
+        if (arrayInt.size <= numberOfItems) return sum(arrayInt).toInt()
+        if (sum(arrayInt) < 0) arrayInt = smallNegativeNumber(arrayInt)
+        return sum(absArray(arrayInt).sorted().drop(arrayInt.size - numberOfItems)).toInt()
+    }
+
+    private fun smallNegativeNumber(array: List<Int>): List<Int> {
+        val arr = array.filter { it < 0 }.sorted()
+        return array.filter { it != arr.last() }
+    }
+
+    private fun sum(array: List<Int>): Long = array.fold(1.toLong()) {
+            previousResult, element -> previousResult * element.toLong()
+    }
+
+    private fun absArray(array: List<Int>): List<Int> {
+        return array.map { abs(it) }
     }
 }
